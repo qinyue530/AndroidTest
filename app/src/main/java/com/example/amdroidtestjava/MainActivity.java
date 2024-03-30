a019b74af6d8a673abc09434be2c16d435814923
qinyue <530495973@qq.com>
2024-03-30 11:08:19 +0800

    设置文本和背景颜色

--- app/src/main/java/com/example/amdroidtestjava/MainActivity.java
+++ app/src/main/java/com/example/amdroidtestjava/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.amdroidtestjava;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,12 +22,23 @@ public class MainActivity extends AppCompatActivity {
 
         TextView helloWorld = findViewById(R.id.helloWorld);
         helloWorld.setText("你好,世界!");
+        //Java设置字体大小默认为sp 会随着系统设置的字体大小改变
+        helloWorld.setTextSize(30);
+        //设置字体颜色
+        helloWorld.setTextColor(Color.WHITE);
+//        helloWorld.setTextColor(0xFF777777);
+        //设置背景颜色
+        helloWorld.setBackgroundColor(Color.RED);
+        //获取x按钮
         Button button = findViewById(R.id.button);
+        //添加按钮点击事件
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
+                //由那个页面,跳转到另一个页面
                 intent.setClass(MainActivity.this,NextActivity.class);
+                //开始跳转
                 startActivity(intent);
             }
         });
