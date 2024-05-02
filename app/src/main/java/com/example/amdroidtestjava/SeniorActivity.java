b4a2893073c2f49f575c57b909e1743f2b8b501f
qinyue <530495973@qq.com>
2024-05-02 09:21:22 +0800

    no message

--- app/src/main/java/com/example/amdroidtestjava/SeniorActivity.java
+++ app/src/main/java/com/example/amdroidtestjava/SeniorActivity.java
@@ -15,7 +15,7 @@ public class SeniorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_senior);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.senior), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
